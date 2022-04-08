28fae42a7e8e88873d62039d67842137065f5bb6
Gustavo Farias <gustavocel82@gmail.com>
2022-04-08 10:18:08 -0300

    first review

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import { nanoid } from 'nanoid';
+import { useEffect, useState } from 'react';
+import Confetti from 'react-confetti';
 import './App.css';
 import ColorBlock from './ColorBlock.js';
-import { useState, useEffect } from 'react';
-import { nanoid } from 'nanoid';
-import Confetti from 'react-confetti'
+
+const LETTERS = ['A', 'B', 'C', 'D', 'E', 'F']
 
 function App() {
   const [swatches, setSwatches] = useState(generateSwatches())
@@ -10,11 +12,15 @@ function App() {
   const [guesses, setGuesses] = useState(0)
   const [win, setWin] = useState(false)
 
-  function  randomColor() {
+  useEffect(() => {
+    setWinningColor(swatches[Math.floor(Math.random() * swatches.length)].color)
+  }, [swatches])
+
+  function randomColor() {
     let color = '#'
     for (let i = 0; i < 6; i++) {
       let value = Math.floor(Math.random() * 9)
-      value % 2 === 0 ? color += value : color += ['A', 'B', 'C', 'D', 'E', 'F'][Math.floor(Math.random() * 6)]
+      color += value % 2 === 0 ? value : LETTERS[Math.floor(Math.random() * 6)];
     }
     return color
   }
@@ -31,19 +37,9 @@ function App() {
     return newSwatches
   }
 
-  useEffect(() => {
-    setWinningColor(swatches[Math.floor(Math.random() * swatches.length)].color)
-  }, [swatches])
-
-
   const checkUserGuess = (swatch) => {
-    if (win) {
-      return false
-    }
-
-    if (swatch.color === winningColor) {
-      setWin(true)
-    }
+    if (win) return false
+    if (swatch.color === winningColor) setWin(true)
 
     toggleHexDisplay(swatch)
     setGuesses((guesses) => guesses += 1)
@@ -59,8 +55,7 @@ function App() {
     setGuesses(0)
   }
 
-  let colors = swatches.map((swatch) => {
-    return(
+  let colors = swatches.map((swatch) => (
       <ColorBlock
         key={swatch.id}
         showHex={swatch.showHex}
@@ -68,16 +63,14 @@ function App() {
         id={swatch.id}
         color={swatch.color} />
     )
-  })
+  )
+
 
   const winningMessage = () => {
-    if (guesses == 1) {
-      return "Winner in 1 guess! All hail the hex master!"
-    } else if (guesses >1 && guesses <=3) {
-      return `You Win with only ${guesses} guesses! That's impressive!`
-    } else if (guesses > 3 && guesses < 5) {
-      return "You Win! Not bad for an amateur."
-    }
+    if (guesses === 1) return "Winner in 1 guess! All hail the hex master!"
+    if (guesses > 1 && guesses <= 3) return `You Win with only ${guesses} guesses! That's impressive!`
+    if (guesses > 3 && guesses < 5) return "You Win! Not bad for an amateur."
+
     return `${guesses} guesses... You win, I guess..`
   }
 
